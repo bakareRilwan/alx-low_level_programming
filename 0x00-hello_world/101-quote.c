1a4aa8be3557742021f6db0a315164a4711b3adc
bakareRilwan <baccrie88@gmail.com>
2022-06-30 16:15:27 -0700

    Fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
-nclude <unistd.h>
+#include <unistd.h>
 /**
- *  * main - prints a standard error
- *   *
- *    * Return: returns 1
- *     */
+ * main - Enter
+ * Return: (1)
+ */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
